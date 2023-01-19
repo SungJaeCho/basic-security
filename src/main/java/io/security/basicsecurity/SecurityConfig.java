fd9e5f7e7dbd8165beef66c99b2b15632ea75ebd
sjchoRsquare <sjcho@rsquare.co.kr>
2023-01-20 08:27:23 +0900

    security 6강 remember me 인증까지

---
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,90 @@
+package io.security.basicsecurity;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired public UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+        ;// 인가정책
+
+        http
+                .formLogin()
+//                .loginPage("/loginPage")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/login")
+//                .usernameParameter("userId")
+//                .passwordParameter("passwd")
+//                .loginProcessingUrl("/login_proc")
+//                .successHandler(new AuthenticationSuccessHandler() {
+//                    @Override
+//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                        System.out.println("authentication : " + authentication.getName());
+//                        response.sendRedirect("/");
+//                    }
+//                })
+//                .failureHandler(new AuthenticationFailureHandler() {
+//                    @Override
+//                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+//                        System.out.println("exception : " + exception.getMessage());
+//                        response.sendRedirect("/login");
+//                    }
+//                })
+//                .permitAll()
+        ;// 인증정책
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate(); //세션 무효화
+
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+//                .deleteCookies("remember-me")
+                // 로그아웃 정책 기본은 POST 방식이나 GET 방식으로도 가능한 방법이 있음
+                .and()
+                .rememberMe()
+                .rememberMeParameter("remember") // 기본 remember-me
+                .tokenValiditySeconds(3600) //기본 14일
+                .userDetailsService(userDetailsService)
+        ;
+
+        return http.build();
+    }
+}
